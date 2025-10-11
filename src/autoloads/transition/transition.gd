11619b50bd2913f94a13e237cbcf39ce1ae6d7b7
UwUMacaroniTime <102434693+UwUMacaroniTime@users.noreply.github.com>
2025-10-11 15:27:42 -0400

    Consume all events during transition

--- src/autoloads/transition/transition.gd
+++ src/autoloads/transition/transition.gd
@@ -4,13 +4,30 @@ extends CanvasLayer
 signal transition_midpoint()
 
 
+var transitioning:bool
+
+
 @onready var animation_player:AnimationPlayer = $AnimationPlayer
 
 
+func _input(_event: InputEvent) -> void:
+	if transitioning:
+		get_viewport().set_input_as_handled()
+
+
 func transition(method:Callable) -> void:
+	transitioning = true
 	animation_player.play(&"transition")
 	transition_midpoint.connect(method, CONNECT_ONE_SHOT)
 
 
-func _transition_callback() -> void:
+func transition_to_file(path:String) -> void:
+	transition(get_tree().change_scene_to_file.bind(path))
+
+
+func _transition_midpoint_callback() -> void:
 	transition_midpoint.emit()
+
+
+func _transition_endpoint_callback() -> void:
+	transitioning = false
