4184ad2486c5289305776d1a627e94a1d1fe01a3
UwUMacaroniTime <102434693+UwUMacaroniTime@users.noreply.github.com>
2025-10-12 15:40:21 -0400

    Add settings manager

---
+++ src/autoloads/settings/settings_manager.gd
@@ -0,0 +1,25 @@
+extends Node
+
+const OPTIONS_SAVE_PATH:String = "user://options.cfg"
+
+var current:SettingsSave
+## Options duplicate used as a scratchboard for all the settings that are not yet
+## applied
+var tenative:SettingsSave
+
+
+func _init() -> void:
+	current = SettingsSave.load_config(OPTIONS_SAVE_PATH)
+	current.apply_settings(get_tree().root)
+	tenative = current.duplicate()
+
+
+func apply_tenative() -> void:
+	current = tenative
+	current.apply_settings(get_tree().root)
+	current.save_config(OPTIONS_SAVE_PATH)
+	tenative = current.duplicate()
+
+
+func cancel_tenative() -> void:
+	tenative = current.duplicate()
