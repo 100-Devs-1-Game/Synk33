6ebfe202ee02ff5b2438986e10f52228a7342b18
UwUMacaroniTime <102434693+UwUMacaroniTime@users.noreply.github.com>
2025-10-22 17:55:25 -0400

    Add playback scrubber

---
+++ src/scenes/menus/vis_playback/vis_playback.gd
@@ -0,0 +1,25 @@
+extends Control
+
+
+@export var player:AudioStreamPlayer
+
+
+@onready var slider:HSlider = $PlaySlider
+
+
+func _process(delta: float) -> void:
+	if player.playing == false:
+		return
+	slider.max_value = player.stream.get_length()
+	slider.set_value_no_signal(player.get_playback_position())
+
+func _on_play_slider_drag_started() -> void:
+	if player.playing == false:
+		return
+	player.stream_paused = true
+
+
+func _on_play_slider_drag_ended(value_changed: bool) -> void:
+	player.stream_paused = false
+	if value_changed:
+		player.seek(slider.value)
