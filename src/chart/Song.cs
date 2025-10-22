4a04f229e0a4ec17dc280c1039dba0cc9777c527
UwUMacaroniTime <102434693+UwUMacaroniTime@users.noreply.github.com>
2025-10-22 17:26:35 -0400

    Basic song select functionality

--- src/chart/Song.cs
+++ src/chart/Song.cs
@@ -1,13 +1,13 @@
-﻿using Godot;
+using Godot;
 using Godot.Collections;
 
 namespace SYNK33.chart;
 
 [GlobalClass]
 public partial class Song : Resource {
-    [Export] public string Name { get; set; }
-    [Export] public string Author { get; set; }
-    [Export] public AudioStream Audio { get; set; }
-    [Export] public Texture2D Cover { get; set; }
-    [Export] public Array<Chart> Charts { get; set; }
+	[Export] public string Name { get; set; }
+	[Export] public string Author { get; set; }
+	[Export] public AudioStream Audio { get; set; }
+	[Export] public Texture2D Cover { get; set; }
+	[Export] public Array<Chart> Charts { get; set; }
 }
