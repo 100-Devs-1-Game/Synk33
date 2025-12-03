5b30d8bc1bfe6e39d97d900eff47d52a43f31641
UwUMacaroniTime <102434693+UwUMacaroniTime@users.noreply.github.com>
2025-12-03 17:06:43 -0500

    Refactor Song/Chart
    ## Update Songs and Charts:
    - Songs can now have only one Chart per Difficulty
    - Songs control BPM information instead of Charts
    - Songs no longer directly reference Charts to allow for lighter loading
    - Charts gain a TempoModifier field that is a multiplier for both song playback speed and BPM
    - Charts gain a reference to their corresponding Song

--- src/chart/Song.cs
+++ src/chart/Song.cs
@@ -9,5 +9,31 @@ public partial class Song : Resource {
 	[Export] public string Author { get; set; }
 	[Export] public AudioStream Audio { get; set; }
 	[Export] public Texture2D Cover { get; set; }
-	[Export] public Array<Chart> Charts { get; set; }
+	[Export] public float Bpm { get; set; }
+	[Export(PropertyHint.Flags, "Easy,Normal,Hard,Expert")] public int Difficulties { get; set; }
+	private static Dictionary<Difficulty, string> DifficultyMap = new Dictionary<Difficulty, string>
+	{
+		{ Difficulty.Easy, "easy" },
+		{ Difficulty.Normal, "normal" },
+		{ Difficulty.Hard, "hard" },
+		{ Difficulty.Expert, "expert" }
+	};
+
+	public Chart? GetChartByDifficulty(Difficulty difficulty)
+	{
+		if ((Difficulties & (int)difficulty) == 0)
+		{
+			return null;
+		}
+
+		string path = $"res://songs/{ResourcePath.GetBaseName()}_{DifficultyMap[difficulty]}.tres";
+		return GD.Load<Chart>(path);
+	}
+	}
+
+public enum Difficulty {
+	Easy = 1,
+	Normal = 1 << 1,
+	Hard = 1 << 2,
+	Expert = 1 << 3
 }
