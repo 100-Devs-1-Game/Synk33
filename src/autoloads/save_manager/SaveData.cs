bdf8e46738641b4edd9aeac853917e8c1ee0fa2f
UwUMacaroniTime <102434693+UwUMacaroniTime@users.noreply.github.com>
2025-12-09 11:45:28 -0500

    Serializing/Deserializing

--- src/autoloads/save_manager/SaveData.cs
+++ src/autoloads/save_manager/SaveData.cs
@@ -1,32 +1,102 @@
+using System;
 using Godot;
 using Godot.Collections;
 
 
 namespace SYNK33.Saving;
 
-interface ISaveInfo {
-    public long GetChartPerformance(long chartUID);
+public struct ChartPerformance {
+    public uint Highscore;
+    public uint PerfectHits;
+    public uint TotalHits;
+    public uint ChartTotalNotes;
 
-    public void SetChartPerformance(long chartUID, long points);
+    public readonly double GetGradeRatio() {
+        return TotalHits / (double)ChartTotalNotes;
+    }
+    public readonly uint GetMisses() {
+        return ChartTotalNotes - TotalHits;
+    }
+}
+
+interface ISaveInfo {
+    public bool HasChartPerformance(long chartUID);
+    public ChartPerformance? GetChartPerformance(long chartUID);
+    public void SetChartPerformance(long chartUID, ChartPerformance chartPerformance);
 }
 
 public partial class SaveData : Resource, ISaveInfo{
+    [ExportGroup("StoryFlags")]
     /// <summary>
     /// Whether the player has completed the tutorial.
     /// </summary>
     [Export] public bool TutorialCompleted = false;
     /// <summary>
-    /// Points highscore of charts by their resource UID.
+    /// Points performance of charts by their resource UID.
     /// </summary>
-    [Export] private Dictionary<long, long> ChartMap = [];
+    private System.Collections.Generic.Dictionary<long, ChartPerformance> ChartMap = [];
 
-    public long GetChartPerformance(long chartUID) {
-        long points = -1;
-        ChartMap.TryGetValue(chartUID, out points);
-        return points;
+    public bool HasChartPerformance(long chartUID) {
+        return ChartMap.ContainsKey(chartUID);
+    }
+    public ChartPerformance? GetChartPerformance(long chartUID) {
+        if (ChartMap.ContainsKey(chartUID)) {
+            return null;
+        }
+        return ChartMap[chartUID];
     }
 
-    public void SetChartPerformance(long chartUID, long points) {
-        ChartMap[chartUID] = points;
+    public void SetChartPerformance(long chartUID, ChartPerformance chartPerformance) {
+        ChartMap[chartUID] = chartPerformance;
+    }
+
+    public void Save(string path) {
+        ConfigFile config = new ConfigFile();
+        config.SetValue("story_flags", "tutorial_completed", TutorialCompleted);
+        config.Save(path);
+    }
+
+    public void Load(string path) {
+        ConfigFile config = new ConfigFile();
+        config.Load(path);
+        TutorialCompleted = (bool)config.GetValue("story_flags", "tutorial_completed", false);
+    }
+
+    public void SerializeChartMap(FileAccess file) {
+        file.Store32((uint)ChartMap.Count);
+        foreach (System.Collections.Generic.KeyValuePair<long, ChartPerformance> kvPerformance in ChartMap) {
+            file.Store64((ulong)kvPerformance.Key);
+            ChartPerformance chartPerformance = kvPerformance.Value;
+            file.Store32(chartPerformance.Highscore);
+            file.Store32(chartPerformance.PerfectHits);
+            file.Store32(chartPerformance.TotalHits);
+            file.Store32(chartPerformance.ChartTotalNotes);
+        }
+    }
+
+    public void DeserializeChartMap(FileAccess file) {
+        uint count = file.Get32();
+        for(uint i = 0; i < count; i++) {
+            long chartUID = (long)file.Get64();
+            ChartPerformance chartPerformance = new ChartPerformance {
+                Highscore = file.Get32(),
+                PerfectHits = file.Get32(),
+                TotalHits = file.Get32(),
+                ChartTotalNotes = file.Get32()
+            };
+            SetChartPerformance(chartUID, chartPerformance);
+        }
+    }
+
+    public void PrintoutChartMap(FileAccess file) {
+        uint count = file.Get32();
+        GD.Print($"Count: {count}");
+        for(uint i = 0; i < count; i++){
+            GD.Print($"({i})\n\tChart UID:{file.Get64()}");
+            GD.Print($"\tHighscore:{file.Get32()}");
+            GD.Print($"\tPerfect Hits:{file.Get32()}");
+            GD.Print($"\tTotal Hits:{file.Get32()}");
+            GD.Print($"\tChart Total Notes:{file.Get32()}");
+        }
     }
 }
