16ec29ff278e12fbc19e1d4ffaa2264726ee5015
UwUMacaroniTime <102434693+UwUMacaroniTime@users.noreply.github.com>
2025-12-09 12:52:58 -0500

    Rename "normal" to "medium"

--- src/chart/Song.cs
+++ src/chart/Song.cs
@@ -11,10 +11,10 @@ public partial class Song : Resource {
 	[Export] public AudioStream Audio { get; set; }
 	[Export] public Texture2D Cover { get; set; }
 	[Export] public float Bpm { get; set; }
-	[Export(PropertyHint.Flags, "Easy,Normal,Hard,Expert")] public int Difficulties { get; set; }
+	[Export(PropertyHint.Flags, "Easy,Medium,Hard,Expert")] public int Difficulties { get; set; }
 	private static Dictionary<Difficulty, string> DifficultyMap = new Dictionary<Difficulty, string> {
 		{ Difficulty.Easy, "easy" },
-		{ Difficulty.Normal, "normal" },
+		{ Difficulty.Medium, "medium" },
 		{ Difficulty.Hard, "hard" },
 		{ Difficulty.Expert, "expert" }
 	};
@@ -35,7 +35,7 @@ public partial class Song : Resource {
 
 public enum Difficulty {
 	Easy,
-	Normal,
+	Medium,
 	Hard,
 	Expert
 }
